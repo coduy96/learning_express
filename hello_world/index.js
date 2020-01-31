f0a799385365ee4d2ae413e34678ddb06116f1da
Duy Co <versco@VersMac.local>
2020-01-31 08:22:24 +0700

    Add create module

--- hello_world/index.js
+++ hello_world/index.js
@@ -1,12 +1,54 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var bodyParse = require('body-parser');
 
 var port = 3000;
 
-app.get('/', function(request, response){
-    response.send('<h1>Hello world</h1>');
+var users = [
+  { id: "1", name: "Duy" },
+  { id: "2", name: "Men" },
+  { id: "3", name: "Ke" }
+];
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({extended: true}));
+
+app.get("/", function(request, response) {
+  //response.send('<h1>Hello world</h1>');
+  response.render("index", {
+    username: "coduy96"
+  });
 });
 
-app.listen(port, function (){
-    console.log('Listenning on port '+port);
+app.get("/users", function(request, response) {
+  response.render("users/index", {
+    users: users
+  });
+});
+
+app.get("/users/search", function(request, response) {
+    var q = request.query.q;
+ 
+    var matchedUser = users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) != -1;
+    });
+
+    response.render('users/index',{
+        users:matchedUser
+    });
+});
+
+app.get("/users/create", function(request, response) {
+  response.render("users/create");
+});
+
+app.post("/users/create", function(request, response) {
+    users.push(request.body);
+    response.redirect("/users");
+});
+
+app.listen(port, function() {
+  console.log("Listenning on port " + port);
 });
